2e846ebef155aa123a53c7b7177bed1a90307b34
rahillakhani <rahil.lakani@in.tesco.com>
2016-08-29 18:47:26 +0530

    implement sass loader.

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// require('./style.sass');
+require('./style.scss');
+class App extends React.Component {
+   render() {
+      return (<div className="center">
+            Hello World!!!
+         </div>);
+   }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+var config = {
+   entry: './main.js',
+   output: {
+      path:'./',
+      filename: 'index.js'
+   },
+   devServer: {
+      inline: true,
+      port: 8080
+   },
+   module: {
+      loaders: [
+      {
+         test: /\.jsx?$/,
+         exclude: /node_modules/,
+         loader: 'babel',
+         query: {
+            presets: ['es2015', 'react']
+         }
+      },
+      {
+         test: /\.scss$/,
+         exclude: /node_modules/,
+         loaders: ["style", "sass", "scss"],
+      }]
+   }
+}
+
+module.exports = config;
